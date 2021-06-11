752beba7f02741186a601ba7261426829eff9087
radamusil <64097743+radamusil@users.noreply.github.com>
2021-06-11 10:27:42 +0200

    first commit

---
+++ resources/views/pages/index.blade copy.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'CRUD')}}</title>
+    </head>
+    <body>
+        <h1>Services</h1>
+        <p>This is the services page</p>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'CRUD')}}</title>
+    </head>
+    <body>
+        <h1>Welcome to Laravel</h1>
+        <p>This is Laravel application</p>
+    </body>
+</html>
